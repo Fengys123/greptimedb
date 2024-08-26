d0d3edd040097abbc34a1e8d00cfd501c7134607
fys <fengys1996@gmail.com>
2024-08-26 15:24:57 +0800

    add intereptor for mito2 ddl handle

--- src/mito2/src/request.rs
+++ src/mito2/src/request.rs
@@ -606,7 +606,7 @@ impl WorkerRequest {
 
 /// DDL request to a region.
 #[derive(Debug)]
-pub(crate) enum DdlRequest {
+pub enum DdlRequest {
     Create(RegionCreateRequest),
     Drop(RegionDropRequest),
     Open((RegionOpenRequest, Option<WalEntryReceiver>)),
@@ -620,13 +620,19 @@ pub(crate) enum DdlRequest {
 
 /// Sender and Ddl request.
 #[derive(Debug)]
-pub(crate) struct SenderDdlRequest {
+pub struct SenderDdlRequest {
     /// Region id of the request.
     pub(crate) region_id: RegionId,
     /// Result sender.
     pub(crate) sender: OptionOutputTx,
     /// Ddl request.
     pub(crate) request: DdlRequest,
+}
+
+impl SenderDdlRequest {
+    pub fn ddl_request(&self) -> &DdlRequest {
+        &self.request
+    }
 }
 
 /// Notification from a background job.
